b1023d00362fe84de42755cf554318aefb65732a
Ragul285 <142964551+Ragul285@users.noreply.github.com>
2023-09-11 12:25:17 +0530

    Create oddeven.java
    odd\even

---
+++ oddeven.java
@@ -0,0 +1,14 @@
+class oddeven{
+public static void main(Strings[]args){
+int num=4567
+if(num%2==0)
+{
+System.out.println("The given"+num+ "is even")
+}
+else
+{
+System.out.println("The given"+num+"is odd")
+}
+}
+
+}
